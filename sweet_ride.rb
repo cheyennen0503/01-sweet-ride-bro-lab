e4d978d23c49be810f05ccc64bec9c3f7ab297de
cheyennen0503 <cheyennen0503@hstat.org>
2017-10-02 15:48:47 +0000

    fix tests for do now

--- sweet_ride.rb
+++ sweet_ride.rb
@@ -1,1 +1,19 @@
-#code your answers here
+def check_it_out
+    return "Check out my sweet new ride, dude!"
+end 
+
+def stolen 
+    return "dude, where's my car?"
+end 
+
+def no_big_deal(car)
+    return "No big deal, it's a #{car}"
+end 
+
+def reporting_theft (car)
+    return "Officer, someone's stolen my #{car}".upcase
+end 
+
+def how_long_until_i_can_get_another_car(price,wage)
+    return price/wage 
+end 
